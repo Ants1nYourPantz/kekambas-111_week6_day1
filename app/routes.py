ed14350082a160fd74d94ffb6a78fe67bc0843dc
Anthony Romano <dentlabpa@gmail.com>
2023-03-20 22:38:27 -0400

    final

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,14 @@
 from app import app
 
-
+from flask import render_template
 
 
 @app.route('/')
 def hello_world():
-    return "hello world"
+    return 'Hello World! Welcome to my blog.'
+
+
+
+@app.route('/favorite_5')
+def fav():
+    return render_template('index.html')
